bd5d2c710b4b149bc94b2b0a938c5aac6c2e71fd
Aaron <toikadeaaron@gmail.com>
2023-10-18 23:05:27 +0100

    zeroDivision error in the division function

---
+++ app/src/main/java/com/example/androiddebug/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.androiddebug
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+private const val  TAG = "MainActivity"
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        logging()
+        division()
+    }
+
+    fun division() {
+        val numerator = 60
+        var denominator = 4
+        repeat(5) {
+            Log.d(TAG, "$denominator")
+            Log.v(TAG, "${numerator / denominator}")
+            denominator--
+        }
+    }
+
+    fun logging() {
+        Log.v(TAG, "Hello world")
+        Log.e(TAG, "ERROR: a serious error like an app crash")
+        Log.w(TAG, "WARN: warns about the potential for serious errors")
+        Log.i(TAG, "INFO: reporting technical information, such as an operation succeeding")
+        Log.d(TAG, "DEBUG: reporting technical information useful for debugging")
+        Log.v(TAG, "VERBOSE: more verbose than DEBUG logs")
+    }
+}
